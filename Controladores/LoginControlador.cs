8891e4a0e3a1e1e171730e88c731fd0e78d1fc1d
vivianacampos <vivianacampos@ugm.cl>
2021-06-09 19:48:16 -0400

    se incluye la base de datos en el proyecto

---
+++ Controladores/LoginControlador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using reciclemos_v2.Clases;
+
+
+namespace reciclemos_v2.Controladores
+{
+    public class LoginControlador
+    {
+        //Método para buscar usuario y contraseña en la BD
+        
+    }
+
+}
