8b3ba873ddf8f45ce1e333f289e72c84068c1d85
vivianacampos <vivianacampos@ugm.cl>
2021-07-07 18:57:11 -0400

    Session Admin

--- WebFormEmpresa/PaginaMaestraEmpresa.Master.cs
+++ WebFormEmpresa/PaginaMaestraEmpresa.Master.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using reciclemos_v2.Clases;
 
 namespace reciclemos_v2
 {
     public partial class PaginaMaestraEmpresa : System.Web.UI.MasterPage
     {
+        int idUsu;
         protected void Page_Load(object sender, EventArgs e)
         {
             loginControl();
+            Usuario usu = new Usuario();
+            if (Session["Admin"] != null)
+            {
+                usu = (Usuario)Session["Admin"];
+                idUsu = usu.IdUsuario;
+                LblUsuario.Text = "Bienvenido/a " + usu.Nombre + " " + usu.Apellido;
+            }
         }
 
         public void loginControl()
