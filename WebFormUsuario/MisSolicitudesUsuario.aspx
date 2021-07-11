4a80995a5fcbe8026fff716cc8a3be5284130ef2
vivianacampos <vivianacampos@ugm.cl>
2021-07-10 22:48:34 -0400

    Diseño Tabla solicitudes

--- WebFormUsuario/MisSolicitudesUsuario.aspx
+++ WebFormUsuario/MisSolicitudesUsuario.aspx
@@ -1,5 +1,50 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/WebFormUsuario/PaginaMaestraUsuario.Master" AutoEventWireup="true" CodeBehind="MisSolicitudesUsuario.aspx.cs" Inherits="reciclemos_v2.WebFormMasterPage.MisSolicitudesUsuario" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container">
+        <h2 class="py-5">Mis Solicitudes</h2>
+        <div class="pb-3 d-flex">
+            <asp:DropDownList ID="DdlFiltrarSol" runat="server" Width="250" CssClass="form-control">
+                <asp:ListItem Selected="True">Todas</asp:ListItem>
+                <asp:ListItem>Recientes</asp:ListItem>
+            </asp:DropDownList>
+            <div class="pl-3">
+                <asp:Button ID="BtnFiltrar" runat="server" Text="Filtrar" Width="100" CssClass="btn btn-primary flex-wrap" />
+            </div>
+        </div>
+        <div class="pt-3">
+            <asp:Table ID="Table1" runat="server" CssClass="table table-hover" style="margin-right: 0px">
+                <asp:TableHeaderRow TableSection="TableHeader" CssClass="table-primary" ForeColor="Gray">
+                    <asp:TableHeaderCell ID="TableHeaderCell1" runat="server">Id Solicitud</asp:TableHeaderCell>
+                    <asp:TableHeaderCell ID="TableHeaderCell2" runat="server">Descripción Materiales</asp:TableHeaderCell>
+                    <asp:TableHeaderCell ID="TableHeaderCell3" runat="server">Estado</asp:TableHeaderCell>
+                    <asp:TableHeaderCell ID="TableHeaderCell4" runat="server">Detalles</asp:TableHeaderCell>
+                    <asp:TableHeaderCell ID="TableHeaderCell5" runat="server">Seleccionar</asp:TableHeaderCell>
+                </asp:TableHeaderRow>
+                <asp:TableRow>
+                    <asp:TableCell ID="Campo1">Hola</asp:TableCell>
+                    <asp:TableCell ID="Campo2">Hola</asp:TableCell>
+                    <asp:TableCell ID="Campo3">Hola</asp:TableCell>
+                    <asp:TableCell ID="Campo4">Hola</asp:TableCell>
+                    <asp:TableCell ID="Campo5">
+                        <asp:CheckBox ID="CheckBox1" runat="server" />
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                </asp:TableRow>
+            </asp:Table>
+        </div>
+        <div class="d-flex">
+            <div class="flex-wrap pr-2">
+                <asp:Button ID="BtnModificar" runat="server" Text="Modificar" CssClass="btn btn-warning" Width="100" />
+            </div>
+            <div class="flex-wrap pr-2">
+                <asp:Button ID="BtnEliminar" runat="server" Text="Eliminar" CssClass="btn btn-danger" Width="100" />
+            </div>
+        </div>
+
+    </div>
+
 </asp:Content>
--- WebFormUsuario/MisSolicitudesUsuario.aspx.designer.cs
+++ WebFormUsuario/MisSolicitudesUsuario.aspx.designer.cs
@@ -13,5 +13,149 @@ namespace reciclemos_v2.WebFormMasterPage
 
     public partial class MisSolicitudesUsuario
     {
+
+        /// <summary>
+        /// Control DdlFiltrarSol.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DdlFiltrarSol;
+
+        /// <summary>
+        /// Control BtnFiltrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnFiltrar;
+
+        /// <summary>
+        /// Control Table1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table Table1;
+
+        /// <summary>
+        /// Control TableHeaderCell1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableHeaderCell TableHeaderCell1;
+
+        /// <summary>
+        /// Control TableHeaderCell2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableHeaderCell TableHeaderCell2;
+
+        /// <summary>
+        /// Control TableHeaderCell3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableHeaderCell TableHeaderCell3;
+
+        /// <summary>
+        /// Control TableHeaderCell4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableHeaderCell TableHeaderCell4;
+
+        /// <summary>
+        /// Control TableHeaderCell5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableHeaderCell TableHeaderCell5;
+
+        /// <summary>
+        /// Control Campo1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell Campo1;
+
+        /// <summary>
+        /// Control Campo2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell Campo2;
+
+        /// <summary>
+        /// Control Campo3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell Campo3;
+
+        /// <summary>
+        /// Control Campo4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell Campo4;
+
+        /// <summary>
+        /// Control Campo5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell Campo5;
+
+        /// <summary>
+        /// Control CheckBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox CheckBox1;
+
+        /// <summary>
+        /// Control BtnModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnModificar;
+
+        /// <summary>
+        /// Control BtnEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnEliminar;
     }
 }
